cdf27f6a75313b188ea65361e39e05f4e63e867d
Pat Nakajima <patnakajima@gmail.com>
2008-11-21 01:03:36 -0500

    moved things to a more RR-esque syntax by using with(JJ) { .. }

--- test/jj_spec.js
+++ test/jj_spec.js
@@ -1,119 +1,121 @@
 Screw.Unit(function() {
-  describe("JJ", function() {
-    var item;
+  with(JJ) {
+    describe("JJ", function() {
+      var item;
     
-    before(function() {
-      item = {
-        foo: function() { return "FOO"; },
-        bar: function() { return "BAR"; }
-      }
-    });
-    
-    it("doesn't affect default behaviors", function() {
-      expect(item.foo()).to(equal, "FOO");
-      expect(item.bar()).to(equal, "BAR");
-    });
-    
-    describe(".stub", function() {
       before(function() {
-        JJ.stub(item).foo.returns("STUBBED");
-      })
-      
-      it("doesn't alter non-stubbed methods", function() {
+        item = {
+          foo: function() { return "FOO"; },
+          bar: function() { return "BAR"; }
+        }
+      });
+    
+      it("doesn't affect default behaviors", function() {
+        expect(item.foo()).to(equal, "FOO");
         expect(item.bar()).to(equal, "BAR");
       });
-      
-      it("changes the stubbed method's return value", function() {
-        expect(item.foo()).to(equal, "STUBBED");
-      });
-      
-      it("can stub multiple methods", function() {
-        JJ.stub(item).bar.returns("OTHER STUBBED");
-        expect(item.foo()).to(equal, "STUBBED");
-        expect(item.bar()).to(equal, "OTHER STUBBED");
-      });
-      
-      it("tracks call counts", function() {
-        JJ.stub(item).bar.returns("OK");
-        expect(JJ.stub(item).bar.callCount()).to(equal, 0);
-        item.bar();
-        expect(JJ.stub(item).bar.callCount()).to(equal, 1);
-        item.bar();
-        expect(JJ.stub(item).bar.callCount()).to(equal, 2);
-      });
-      
-      describe("with a context", function() {
+    
+      describe(".stub", function() {
         before(function() {
-          JJ.stub(item, function(stub) {
-            stub.foo.returns("CONTEXT-FOO")
-            stub.fizz.returns("FIZZ");
-            stub.buzz.returns("BUZZ");
+          stub(item).foo.returns("STUBBED");
+        })
+      
+        it("doesn't alter non-stubbed methods", function() {
+          expect(item.bar()).to(equal, "BAR");
+        });
+      
+        it("changes the stubbed method's return value", function() {
+          expect(item.foo()).to(equal, "STUBBED");
+        });
+      
+        it("can stub multiple methods", function() {
+          stub(item).bar.returns("OTHER STUBBED");
+          expect(item.foo()).to(equal, "STUBBED");
+          expect(item.bar()).to(equal, "OTHER STUBBED");
+        });
+      
+        it("tracks call counts", function() {
+          stub(item).bar.returns("OK");
+          expect(stub(item).bar.callCount()).to(equal, 0);
+          item.bar();
+          expect(stub(item).bar.callCount()).to(equal, 1);
+          item.bar();
+          expect(stub(item).bar.callCount()).to(equal, 2);
+        });
+      
+        describe("with a context", function() {
+          before(function() {
+            stub(item, function(stub) {
+              stub.foo.returns("CONTEXT-FOO")
+              stub.fizz.returns("FIZZ");
+              stub.buzz.returns("BUZZ");
+            });
+          });
+
+          it("allows stub declarations on function argument", function() {
+            expect(item.foo()).to(equal, "CONTEXT-FOO");
+          });
+
+          it("allows stubbing of undefined methods", function() {
+            expect(item.fizz()).to(equal, "FIZZ");
+            expect(item.buzz()).to(equal, "BUZZ");
+          });
+
+          it("resets dynamically added stub context methods", function() {
+            reset(item);
+            expect(item.fizz).to(be_undefined);
+            expect(item.buzz).to(be_undefined);
           });
         });
-
-        it("allows stub declarations on function argument", function() {
-          expect(item.foo()).to(equal, "CONTEXT-FOO");
+      });
+    
+      describe(".reset", function() {
+        before(function() {
+          stub(item).foo.returns("STUBBED");
         });
-
-        it("allows stubbing of undefined methods", function() {
+      
+        it("resets an item", function() {
+          reset(item);
+          expect(item.bar()).to(equal, "BAR")
+          expect(item.foo()).to(equal, "FOO");
+        });
+      
+        it("resets item when multiple methods are stubbed", function() {
+          stub(item).bar.returns("OTHER STUBBED");
+          reset(item);
+          expect(item.bar()).to(equal, "BAR")
+          expect(item.foo()).to(equal, "FOO");
+        });
+      
+        it("doesn't blow up when attempting to reset non-stubbed object", function() {
+          reset({ }); // should not throw error
+        });
+      });
+    
+      describe("#method", function() {
+        before(function() {
+          stub(item).method("foo").returns("METHOD-FOO");
+          stub(item).method("fizz").returns("FIZZ");
+        })
+      
+        it("allows stubbing for pre-defined methods", function() {
+          expect(item.foo()).to(equal, 'METHOD-FOO');
+        });
+      
+        it("allows stubbing for undefined methods", function() {
           expect(item.fizz()).to(equal, "FIZZ");
-          expect(item.buzz()).to(equal, "BUZZ");
         });
-
-        it("resets dynamically added stub context methods", function() {
-          JJ.reset(item);
+      
+        it("gets reset", function() {
+          reset(item);
+          expect(item.foo()).to(equal, "FOO");
           expect(item.fizz).to(be_undefined);
-          expect(item.buzz).to(be_undefined);
         });
       });
-    });
     
-    describe(".reset", function() {
-      before(function() {
-        JJ.stub(item).foo.returns("STUBBED");
-      });
-      
-      it("resets an item", function() {
-        JJ.reset(item);
-        expect(item.bar()).to(equal, "BAR")
-        expect(item.foo()).to(equal, "FOO");
-      });
-      
-      it("resets item when multiple methods are stubbed", function() {
-        JJ.stub(item).bar.returns("OTHER STUBBED");
-        JJ.reset(item);
-        expect(item.bar()).to(equal, "BAR")
-        expect(item.foo()).to(equal, "FOO");
-      });
-      
-      it("doesn't blow up when attempting to reset non-stubbed object", function() {
-        JJ.reset({ }); // should not throw error
+      after(function() {
+        reset(item);
       });
     });
-    
-    describe("#method", function() {
-      before(function() {
-        JJ.stub(item).method("foo").returns("METHOD-FOO");
-        JJ.stub(item).method("fizz").returns("FIZZ");
-      })
-      
-      it("allows stubbing for pre-defined methods", function() {
-        expect(item.foo()).to(equal, 'METHOD-FOO');
-      });
-      
-      it("allows stubbing for undefined methods", function() {
-        expect(item.fizz()).to(equal, "FIZZ");
-      });
-      
-      it("gets reset", function() {
-        JJ.reset(item);
-        expect(item.foo()).to(equal, "FOO");
-        expect(item.fizz).to(be_undefined);
-      });
-    });
-    
-    after(function() {
-      JJ.reset(item);
-    });
-  });
+  }
 });
--- test/screw_jj_spec.js
+++ test/screw_jj_spec.js
@@ -1,45 +1,47 @@
 Screw.Unit(function(screw) { with(screw) {
-  describe("JJ Screw.Unit integration", function() {
-    var item;
+  with(JJ) {
+    describe("JJ Screw.Unit integration", function() {
+      var item;
     
-    before(function() {
-      JJ.reset(item);
-      item = {
-        foo: function() { return "FOO"; },
-        bar: function() { return "BAR"; }
-      }
+      before(function() {
+        reset(item);
+        item = {
+          foo: function() { return "FOO"; },
+          bar: function() { return "BAR"; }
+        }
+      });
+    
+      it("verifies mocks were called", function() {
+        mock(item).method('foo');
+        item.foo();
+      });
+    
+      it("allows limit on times called", function() {
+        mock(item).foo.times(2)
+        item.foo();
+        item.foo();
+      });
+    
+      it("keeps a call count", function() {
+        mock(item).foo.returns('foo');
+        expect(mock(item).foo.callCount()).to(equal, 0);
+        item.foo();
+        expect(mock(item).foo.callCount()).to(equal, 1);
+        item.foo();
+        expect(mock(item).foo.callCount()).to(equal, 2);
+        item.bar();
+        expect(mock(item).foo.callCount()).to(equal, 2);
+      });
+    
+      it("can be called off by resetting", function() {
+        mock(item).foo.times(3);
+        reset(item);
+      })
+    
+      after(function() {
+        verify(screw);
+        reset(item);
+      });
     });
-    
-    it("verifies mocks were called", function() {
-      JJ.mock(item).method('foo');
-      item.foo();
-    });
-    
-    it("allows limit on times called", function() {
-      JJ.mock(item).foo.times(2)
-      item.foo();
-      item.foo();
-    });
-    
-    it("keeps a call count", function() {
-      JJ.mock(item).foo.returns('foo');
-      expect(JJ.mock(item).foo.callCount()).to(equal, 0);
-      item.foo();
-      expect(JJ.mock(item).foo.callCount()).to(equal, 1);
-      item.foo();
-      expect(JJ.mock(item).foo.callCount()).to(equal, 2);
-      item.bar();
-      expect(JJ.mock(item).foo.callCount()).to(equal, 2);
-    });
-    
-    it("can be called off by resetting", function() {
-      JJ.mock(item).foo.times(3);
-      JJ.reset(item);
-    })
-    
-    after(function() {
-      JJ.verify(screw);
-      JJ.reset(item);
-    });
-  });
+  }
 }});
